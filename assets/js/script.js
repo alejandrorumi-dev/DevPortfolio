9d87a2f7fad87f17ac39833d1a3604369d775cf5
alejandrorumi-dev <alejandrorumi.dev@gmail.com>
2025-04-11 09:53:41 +0200

    New button to scroll to top

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+/** Botón de menu */
+
 document.querySelector('.mobile-menu-btn').addEventListener('click', function () {
 	document.querySelector('body').classList.toggle('mobile-menu-active');
+});
+
+/**Botón de volver arriba */
+
+const btnVolverArriba = document.getElementById('volver-arriba');
+
+window.addEventListener('scroll', () => {
+	if (window.scrollY > 200) {
+		btnVolverArriba.classList.add('mostrar');
+	} else {
+		btnVolverArriba.classList.remove('mostrar');
+	}
+});
+
+btnVolverArriba.addEventListener('click', () => {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth'
+	});
 });
